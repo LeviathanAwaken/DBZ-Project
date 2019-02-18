97292bfa036510d9e66775fa7d10b75f2a161c08
Reactionz <lmarquez12@csub.edu>
2019-02-18 14:15:37 -0800

    Update lawrenceM.cpp

--- lawrenceM.cpp
+++ lawrenceM.cpp
@@ -1,14 +1,6 @@
 //3350
 // Lawrence Marquez
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <time.h>
-#include <math.h>
-#include <X11/Xlib.h>
-#include <X11/keysym.h>
 #include <GL/glx.h>
 #include "fonts.h"
 
