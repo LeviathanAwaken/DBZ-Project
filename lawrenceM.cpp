d8a1cd4e42f1fdf0dc53ab75983b252484ec52ec
Reactionz <lmarquez12@csub.edu>
2019-02-18 14:17:20 -0800

    Update lawrenceM.cpp

--- lawrenceM.cpp
+++ lawrenceM.cpp
@@ -17,18 +17,13 @@ void showLawrenceText(int x, int y)
 
 void showLawrencePicture (int x, int y, GLuint textid)
 {
-    
     glBindTexture(GL_TEXTURE_2D, textid);
-
     glBegin(GL_QUADS);
         glTexCoord2f(0.0f, 1.0f); glVertex2i(x+525,y+250);
         glTexCoord2f(0.0f, 0.0f); glVertex2i(x+525,y+500);
         glTexCoord2f(1.0f, 0.0f); glVertex2i(x+675,y+500);
         glTexCoord2f(1.0f, 1.0f); glVertex2i(x+675,y+500);
-
     glEnd();
-  
-
 }
 void showLawrence(int x, int y,  GLuint textint)
 {
