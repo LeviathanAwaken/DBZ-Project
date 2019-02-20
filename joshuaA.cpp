a9c61f20921067b145fd267781d9d0d9e7a1a428
Student <student@lab-311-012.cs.csubak.edu>
2019-02-20 11:16:56 -0800

    fixed my image

--- joshuaA.cpp
+++ joshuaA.cpp
@@ -13,12 +13,13 @@ void showJoshuaText(int x, int y)
     r.bot = y-20;
     r.left = x;
     r.center = 0;
-    ggprint8b(&r, 16, c, "Joshua Annis");
+    ggprint8b(&r, 22, c, "Joshua Annis");
 }
 
 void showJoshuaPic(int x, int y, GLuint textInt)
 {
     glBindTexture(GL_TEXTURE_2D, textInt);
+    glColor4f(1, 1, 1, 1);
     glBegin(GL_QUADS);
     glTexCoord2f(0.0f, 1.0f); glVertex2i(x+525, y+250); //bottom left
     glTexCoord2f(0.0f, 0.0f); glVertex2i(x+525, y+500); //top left
