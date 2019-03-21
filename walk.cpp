1e737bd4ecc0090c5b9cc53a68192aa6a1d0758f
Reactionz <lmarquez12@csub.edu>
2019-03-20 20:11:55 -0700

    .

--- walk.cpp
+++ walk.cpp
@@ -48,31 +48,6 @@ SpriteSheet img[] =
     SpriteSheet("images/kiBlast.png", 3, 1), 
     SpriteSheet("images/namek.gif", 4,1)
 };
-    
-
-//-----------------------------------------------------------------------------
-//Setup timers
-// class Timers {
-//     public:
-//         double physicsRate;
-//         double oobillion;
-//         struct timespec timeStart, timeEnd, timeCurrent;
-//         struct timespec walkTime;
-//         Timers() {
-//             physicsRate = 1.0 / 30.0;
-//             oobillion = 1.0 / 1e9;
-//         }
-//         double timeDiff(struct timespec *start, struct timespec *end) {
-//             return (double)(end->tv_sec - start->tv_sec ) +
-//                 (double)(end->tv_nsec - start->tv_nsec) * oobillion;
-//         }
-//         void timeCopy(struct timespec *dest, struct timespec *source) {
-//             memcpy(dest, source, sizeof(struct timespec));
-//         }
-//         void recordTime(struct timespec *t) {
-//             clock_gettime(CLOCK_REALTIME, t);
-//         }
-// } timers;
 
 class X11_wrapper {
     private:
