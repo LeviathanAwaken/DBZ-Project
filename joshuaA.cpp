05177b86940c9e49c2f1af2405568f7eb3fd6abc
dfloydCSUB <dfloyd1@csub.edu>
2019-04-05 13:13:26 -0700

    update for powerups

--- joshuaA.cpp
+++ joshuaA.cpp
@@ -9,25 +9,24 @@
 #include <vector>
 #include <cmath>
 #include "Global.h"
-/*#include "Powerups.h"*/
+#include "Powerups.h"
 /* 
  * My file includes the background to the game and
  * my own personal picture in the credits.
  *
  */
 extern Global g;
-//Powerups powerups[3];
-//void pattern_1(Powerups&);
+Powerups powerups;
 //------------------Background----------------------------------
 void setBackgroundNamek(int x, int y, GLuint textInt)
 {
     glBindTexture(GL_TEXTURE_2D, textInt);
     glColor4f(1, 1, 1, 1);
     glBegin(GL_QUADS); 
-    glTexCoord2f(0.0f, 1.0f); glVertex2i(x+ 0, y+0); //bottom left
-    glTexCoord2f(0.0f, 0.0f); glVertex2i(x+ 0, y+540); //top left
-    glTexCoord2f(1.0f, 0.0f); glVertex2i(x+ 960, y+540); //top right
-    glTexCoord2f(1.0f, 1.0f); glVertex2i(x+ 960, y+0);  
+    glTexCoord2f(0.0f, 1.0f); glVertex2i(0, 35); //bottom left
+    glTexCoord2f(0.0f, 0.0f); glVertex2i(0, y); //top left
+    glTexCoord2f(1.0f, 0.0f); glVertex2i(x, y); //top right
+    glTexCoord2f(1.0f, 1.0f); glVertex2i(x, 35);  
     glEnd();
 }
 //--------------------------------------------------------------
@@ -60,39 +59,29 @@ void showJoshua(int x, int y, GLuint textInt)
 
 }
 //----------------------Power-ups----------------------------
-/*void Powerups_init ()
+void Powerups_init ()
 {
     srand(time(NULL));
-    for (int i = 0; i < 3; i++) {
-	powerups[i].wavepos = (rand() % g.yres);
-	int choice = (rand() % 4 + 1);
-	[i].pattern = choice;
-	powerups[i].pos[0] = g.xres + (rand() % 100);
-	powerups[i].pos[1] = (rand() % g.yres); 
-    }
+	powerups.pos[0] = g.xres + (rand() % 100);
+	powerups.pos[1] = (rand() % g.yres); 
 }
+
 
 void powerupsPhysics ()
 {
-    for (int i = 0; i < 3; i++) {
-	if(enemy[i].pattern == 1)
-	    pattern_1(enemy[i]);
-    }
+   powerups.pos[0] += 2;
 }
 
-void powerupsRender (GLuint image)
+void powerupsRender ()
 {
-
-    for (int i = 0; i < 3; i++) {
 	glPushMatrix();
-	glTranslated(powerups[i].pos[0], powerups[i].pos[1], powerups[i].pos[2]);
+	glTranslated(powerups.pos[0], powerups.pos[1], powerups.pos[2]);
 	glColor3f(1.0, 1.0, 1.0);
-	glBindTexture(GL_TEXTURE_2D, image);
+	//BindTexture(GL_TEXTURE_2D, image);
 	
 	glEnable(GL_ALPHA_TEST);
 	glAlphaFunc(GL_GREATER, 0.0f);
 	glColor4ub(255,255,255,255);
-
 
 	float tx = 0, ty = 0;
 
@@ -104,13 +93,11 @@ void powerupsRender (GLuint image)
 	glTexCoord2f(tx, ty+1);   glVertex2i(70, 0);
 	glEnd();
 
-
-
 	glPopMatrix();
 
 	glBindTexture(GL_TEXTURE_2D, 0);
 	glDisable(GL_ALPHA_TEST);
-    }
+    
 
 }
-*/
+
