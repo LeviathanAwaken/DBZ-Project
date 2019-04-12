0cc6432d6c885ee09e17917eb2afaa29004badb9
Sean <sean.fontes@gmail.com>
2019-04-12 10:16:33 -0700

    Implemented Collision
    Fixed rendering for goku and kiblasts. Collision implemented and working.

--- drakeF.cpp
+++ drakeF.cpp
@@ -17,7 +17,7 @@
 
 extern Global g;
 Enemy enemy[10];
-extern void enemyReference(Enemy&);
+extern void enemyReference(Enemy *);
 float nticks = 0.0;
 int count = 1;
 void Enemy_init();
@@ -30,155 +30,153 @@ void pattern_4(Enemy&);
 
 void showdrakeText(int x, int y)
 {
-    Rect r;
-    unsigned int c = 0x00ffff44;
-    r.bot = y-20;
-    r.left = x;
-    r.center = 0;
-    ggprint8b(&r, 16, c, "Drake Floyd");
+	Rect r;
+	unsigned int c = 0x00ffff44;
+	r.bot = y-20;
+	r.left = x;
+	r.center = 0;
+	ggprint8b(&r, 16, c, "Drake Floyd");
 }
 
 void showdrakePic(int x, int y, GLuint textInt)
 {
-    glBindTexture(GL_TEXTURE_2D, textInt);
-    glBegin(GL_QUADS);
-        glTexCoord2f(0.0f, 1.0f); glVertex2i(x+325, y+150); //bottom left
-        glTexCoord2f(0.0f, 0.0f); glVertex2i(x+325, y+400); //top left
-        glTexCoord2f(1.0f, 0.0f); glVertex2i(x+475, y+400); //top right
-        glTexCoord2f(1.0f, 1.0f); glVertex2i(x+475, y+150); //bottom right
-    glEnd();
+	glBindTexture(GL_TEXTURE_2D, textInt);
+	glBegin(GL_QUADS);
+		glTexCoord2f(0.0f, 1.0f); glVertex2i(x+325, y+150); //bottom left
+		glTexCoord2f(0.0f, 0.0f); glVertex2i(x+325, y+400); //top left
+		glTexCoord2f(1.0f, 0.0f); glVertex2i(x+475, y+400); //top right
+		glTexCoord2f(1.0f, 1.0f); glVertex2i(x+475, y+150); //bottom right
+	glEnd();
 }
 
 void showDrake(int x, int y, GLuint textInt)
 {
-    showdrakePic(0, 0, textInt);
-    showdrakeText(x+300, y+250);
+	showdrakePic(0, 0, textInt);
+	showdrakeText(x+300, y+250);
 
 }
 
 //-----------------------------movement for enemy work in progress-----------------------------
 void Enemy_init ()
-{   
-    srand(time(NULL));
-    for (int i = 0; i < 1; i++) {
-        enemy[i].wavepos = (rand() % g.yres);
-        int choice = (rand() % 4 + 1);
-        enemy[i].pattern = choice;
-        enemy[i].pos[0] = g.xres + (rand() % 100);
-        enemy[i].pos[1] = (rand() % g.yres);
-        enemyReference(enemy[i]);
-    }
+{
+	srand(time(NULL));
+	for (int i = 0; i < 3; i++) {
+		enemy[i].wavepos = (rand() % g.yres);
+		int choice = (rand() % 4 + 1);
+		enemy[i].pattern = choice;
+		enemy[i].pos[0] = g.xres + (rand() % 100);
+		enemy[i].pos[1] = (rand() % g.yres);
+		enemyReference(&enemy[i]);
+	}
 }
 
-void saibaPhysics () 
+void saibaPhysics ()
 {
-    
 
-    for (int i = 0; i < 1; i++) {
-        if(enemy[i].pattern == 1)
-            pattern_1(enemy[i]);
-        if(enemy[i].pattern == 2)
-            pattern_2(enemy[i]);
-        if(enemy[i].pattern == 3)
-            pattern_3(enemy[i]);
-        /*if(enemy[i].pattern == 4)
-            pattern_4(enemy[i]);*/
-    }
-  
+
+	for (int i = 0; i < 1; i++) {
+		if(enemy[i].pattern == 1)
+			pattern_1(enemy[i]);
+		if(enemy[i].pattern == 2)
+			pattern_2(enemy[i]);
+		if(enemy[i].pattern == 3)
+			pattern_3(enemy[i]);
+		/*if(enemy[i].pattern == 4)
+			pattern_4(enemy[i]);*/
+	}
+
 }
 
-void saibaRender (GLuint image) 
+void saibaRender (GLuint image)
 {
-    for (int i = 0; i < 1; i++) {
-        glPushMatrix();
-        glTranslated(enemy[i].pos[0], enemy[i].pos[1], enemy[i].pos[2]);
-        glColor3f(1.0, 1.0, 1.0);
-        glBindTexture(GL_TEXTURE_2D, image);
-        //
-        glEnable(GL_ALPHA_TEST);
-        glAlphaFunc(GL_GREATER, 0.0f);
-        glColor4ub(255,255,255,255);
+	for (int i = 0; i < 1; i++) {
+		glPushMatrix();
+		glTranslated(enemy[i].pos[0], enemy[i].pos[1], enemy[i].pos[2]);
+		glColor3f(1.0, 1.0, 1.0);
+		glBindTexture(GL_TEXTURE_2D, image);
+		//
+		glEnable(GL_ALPHA_TEST);
+		glAlphaFunc(GL_GREATER, 0.0f);
+		glColor4ub(255,255,255,255);
 
 
-            float tx = 0, ty = 0;
+			float tx = 0, ty = 0;
 
-            // Render saibamen
-            glBegin(GL_QUADS);
-            glTexCoord2f(tx+1, ty+1); glVertex2i(0, 0);
-            glTexCoord2f(tx+1, ty);   glVertex2i(0, 50);
-            glTexCoord2f(tx, ty);     glVertex2i(70, 50);
-            glTexCoord2f(tx, ty+1);   glVertex2i(70, 0);
-            glEnd();
+			// Render saibamen
+			glBegin(GL_QUADS);
+			glTexCoord2f(tx+1, ty+1); glVertex2i(0, 0);
+			glTexCoord2f(tx+1, ty);   glVertex2i(0, 50);
+			glTexCoord2f(tx, ty);     glVertex2i(70, 50);
+			glTexCoord2f(tx, ty+1);   glVertex2i(70, 0);
+			glEnd();
 
 
 
-            glPopMatrix();
+			glPopMatrix();
 
-        glBindTexture(GL_TEXTURE_2D, 0);
-        glDisable(GL_ALPHA_TEST);
-        
-       
-    }
+		glBindTexture(GL_TEXTURE_2D, 0);
+		glDisable(GL_ALPHA_TEST);
+
+
+	}
 
 }
 
 //------------------------Draw the enemies-----------------------------------------
 
 void enemyHandler (GLuint image) {
-   
-        saibaRender(image);
+
+		saibaRender(image);
 }
 
 //--------------------enemy attack patterns----------------------------------------
 
 void pattern_1 (Enemy &e)
 {
-        e.pos[0] -= 1;
-        if (e.pos[0] < -50){
-            e.pos[0] = g.xres;
-            e.pos[1] = (rand() % (g.yres-100) + 1);
-        }
+		e.pos[0] -= 1;
+		if (e.pos[0] < -50){
+			e.pos[0] = g.xres;
+			e.pos[1] = (rand() % (g.yres-100) + 1);
+		}
 }
 
 void pattern_2 (Enemy &e)
 {
-    
-        srand(time(NULL));
-        //int start = (rand() % g.yres + 1);
-        nticks+= 0.3;
-        e.pos[0] -= 1;
-        e.pos[1] = (50 * sin(nticks/50) + (e.wavepos));
 
-        if (e.pos[0] < -50){
-            e.pos[0] = g.xres;
-            e.wavepos = (rand() % g.yres + 1);
-            
-        }
-    
+		srand(time(NULL));
+		//int start = (rand() % g.yres + 1);
+		nticks+= 0.3;
+		e.pos[0] -= 1;
+		e.pos[1] = (50 * sin(nticks/50) + (e.wavepos));
+
+		if (e.pos[0] < -50){
+			e.pos[0] = g.xres;
+			e.wavepos = (rand() % g.yres + 1);
+
+		}
+
 }
 
 void pattern_3 (Enemy &e)
 {
-        e.pos[0] -= 1;
-        while (e.pos[1] < (g.yres) + 10) {
-            e.pos[1] += 1;
-        }
-        if (e.pos[0] < -50){
-            e.pos[0] = g.xres;
-            e.pos[1] = (rand() % g.yres + 1);    
-            
-        }
+		e.pos[0] -= 1;
+		while (e.pos[1] < (g.yres) + 10) {
+			e.pos[1] += 1;
+		}
+		if (e.pos[0] < -50){
+			e.pos[0] = g.xres;
+			e.pos[1] = (rand() % g.yres + 1);
+
+		}
 }
 
-void pattern_4 (Enemy &e) 
+void pattern_4 (Enemy &e)
 {
-    e.pos[0] -= 1;
-    e.pos[1] = (e.pos[0] * e.pos[0]);
-    if (e.pos[0] < -50){
-            e.pos[0] = g.xres;
-            e.pos[1] = (rand() % g.yres + 1);
-            
-        }
+	e.pos[0] -= 1;
+	e.pos[1] = (e.pos[0] * e.pos[0]);
+	if (e.pos[0] < -50){
+			e.pos[0] = g.xres;
+			e.pos[1] = (rand() % g.yres + 1);
+
+		}
 }
-
-
--- seanF.cpp
+++ seanF.cpp
@@ -21,155 +21,255 @@
 #include <cmath>
 #include <vector>
 #include "Enemy.h"
+#ifdef SOUND
+	#include </usr/include/AL/alut.h>
+	#include <unistd.h>
+#endif
 
 const int MAX_KI = 10;
 const int UNASSIGN = -5000;
-Enemy *enemyRef[3];
+const int COLLISION = 2;
+const int MAX_ENEM = 3;
+
+Enemy *enemyRef[MAX_ENEM];
 int limiter = 0;
+
+void kiCollision(int);
+void gokuRender();
+void kiHandler(int);
 
 //File tracks x and y resolutions to prevent repassing them in functions.
 class Glob {
-    public:
-        int xres, yres;
+	public:
+		int xres, yres;
 } glob;
+
+class Protag {
+	public:
+		GLuint pic;
+		int pos[2];
+		int vel[2];
+		int height;
+		int width;
+} goku;
 
 //Class to track x and y positions of the blasts being sent by the character.
 class kiBlast {
-    public:
-        GLuint image;
-        int kiTracker[MAX_KI][2];
-        int kiVel;
+	public:
+		GLuint image;
+		int kiTracker[MAX_KI][2];
+		int kiVel;
 } ki;
 
-void enemyReference(Enemy &enem)
+void enemyReference(Enemy* enem)
 {
-    switch (limiter) {
-        case 0:
-            enemyRef[0] = &enem;
-            break;
-        case 1:
-            enemyRef[1] = &enem;
-            break;
-        case 2:
-            enemyRef[2] = &enem;
-            break;
-    }
-    limiter++;
+	switch (limiter) {
+		case 0:
+			enemyRef[0] = enem;
+			break;
+		case 1:
+			enemyRef[1] = enem;
+			break;
+		case 2:
+			enemyRef[2] = enem;
+			break;
+	}
+	limiter++;
 }
 
 //Initializes the kiClass for use.
 void kiInit()
 {
-    ki.kiVel = 2;
-    for (int i = 0; i < MAX_KI; i++) {
-        ki.kiTracker[i][0] = UNASSIGN;
-        ki.kiTracker[i][1] = UNASSIGN;
-    }
+	ki.kiVel = 2;
+	for (int i = 0; i < MAX_KI; i++) {
+		ki.kiTracker[i][0] = UNASSIGN;
+		ki.kiTracker[i][1] = UNASSIGN;
+	}
+}
+
+void gokuInit()
+{
+	goku.vel[0] = 0;
+	goku.vel[1] = 0;
+	goku.height = 70;
+	goku.width = 100;
+	goku.pos[0] = glob.xres / 2 - (goku.width / 2);
+	goku.pos[1] = glob.yres / 2 - (goku.height / 2);
 }
 
 //Generalized initializer for the file, called in the main file.
-void sInit(GLuint image, int xres, int yres)
+void sInit(GLuint gok, GLuint image, int xres, int yres)
 {
-    ki.image= image;
-    glob.xres = xres;
-    glob.yres = yres;
-    kiInit();
-
+	goku.pic = gok;
+	ki.image = image;
+	glob.xres = xres;
+	glob.yres = yres;
+	gokuInit();
+	kiInit();
 }
 
 void showText(int x, int y)
 {
-    Rect r;
-    unsigned int c = 0x00ffff44;
-    r.bot = y-20;
-    r.left = x;
-    r.center = 0;
-    ggprint8b(&r, 16, c, "Sean Fontes");
+	Rect r;
+	unsigned int c = 0x00ffff44;
+	r.bot = y-20;
+	r.left = x;
+	r.center = 0;
+	ggprint8b(&r, 16, c, "Sean Fontes");
 }
 
 void showPic(int x, int y, GLuint textInt)
 {
-    glBindTexture(GL_TEXTURE_2D, textInt);
-    glColor4f(1, 1, 1, 1);
-    glBegin(GL_QUADS);
-        glTexCoord2f(0.0f, 1.0f); glVertex2i(x+100, y+100);
-        glTexCoord2f(0.0f, 0.0f); glVertex2i(x+100, y+250);
-        glTexCoord2f(1.0f, 0.0f); glVertex2i(x+250, y+250);
-        glTexCoord2f(1.0f, 1.0f); glVertex2i(x+250, y+100);
-    glEnd();
+	glBindTexture(GL_TEXTURE_2D, textInt);
+	glColor4f(1, 1, 1, 1);
+	glBegin(GL_QUADS);
+		glTexCoord2f(0.0f, 1.0f); glVertex2i(x+100, y+100);
+		glTexCoord2f(0.0f, 0.0f); glVertex2i(x+100, y+250);
+		glTexCoord2f(1.0f, 0.0f); glVertex2i(x+250, y+250);
+		glTexCoord2f(1.0f, 1.0f); glVertex2i(x+250, y+100);
+	glEnd();
 }
 
 void showSean(int x, int y, GLuint textInt)
 {
-    showPic(0, 0, textInt);
-    showText(x+100, y+100);
+	showPic(0, 0, textInt);
+	showText(x+100, y+100);
+}
+
+void velUpd(int key)
+{
+	switch (key) {
+		case 0:
+			goku.vel[0]--;
+			break;
+		case 1:
+			goku.vel[0]++;
+			break;
+		case 2:
+			goku.vel[1]++;
+			break;
+		case 3:
+			goku.vel[1]--;
+			break;
+	}
+}
+
+void gokuMove()
+{
+	if ((goku.pos[0] > 0 && goku.vel[0] < 0)
+	|| (goku.pos[0] < (g.xres - goku.width) && goku.vel[0] > 0))
+		goku.pos[0] += goku.vel[0];
+	else
+		goku.vel[0] = 0;
+	if ((goku.pos[1] > 0 && goku.vel[1] < 0)
+	|| (goku.pos[1] < (g.yres - goku.height) && goku.vel[1] > 0))
+		goku.pos[1] += goku.vel[1];
+	else
+		goku.vel[1] = 0;
+}
+
+void sRender()
+{
+	gokuRender();
+	kiHandler(1);
+}
+
+void gokuRender()
+{
+	glPushMatrix();
+
+	glTranslatef(goku.pos[0], goku.pos[1], 0);
+	glColor3f(1.0, 1.0, 1.0);
+	glBindTexture(GL_TEXTURE_2D, g.walkTexture);
+	//
+	glEnable(GL_ALPHA_TEST);
+	glAlphaFunc(GL_GREATER, 0.0f);
+	glColor4ub(255,255,255,255);
+
+	// CHANGED
+	int ix = 0;
+	int iy = 0;
+	if (g.walkFrame >= 8)
+		iy = 1;
+
+	float tx = (float)ix;
+	float ty = (float)iy;
+
+	glBegin(GL_QUADS);
+		glTexCoord2f(tx+1, ty+1); 	glVertex2i(0, 0);
+		glTexCoord2f(tx+1, ty);    	glVertex2i(0, goku.height);
+		glTexCoord2f(tx, ty);    	glVertex2i(goku.width, goku.height);
+		glTexCoord2f(tx, ty+1); 	glVertex2i(goku.width, 0);
+	glEnd();
+	glPopMatrix();
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glDisable(GL_ALPHA_TEST);
 }
 
 //Checks ki position against window bounds.
 int kiLimitCheck()
 {
-    for (int i = 0; i < MAX_KI; i++) {
-        if (ki.kiTracker[i][0] == UNASSIGN && ki.kiTracker[i][1] == UNASSIGN)
-            return i;
-    }
-    return -1;
+	for (int i = 0; i < MAX_KI; i++) {
+		if (ki.kiTracker[i][0] == UNASSIGN && ki.kiTracker[i][1] == UNASSIGN)
+			return i;
+	}
+	return -1;
 }
 
 //Creates a kiBlast, if it's not at max.
-void launchKi(int x, int y)
+void launchKi()
 {
-    int kiNum = kiLimitCheck();
-    if (kiNum != -1) {
-        ki.kiTracker[kiNum][0] = x;
-        ki.kiTracker[kiNum][1] = y;
-    }
+	int kiNum = kiLimitCheck();
+	if (kiNum != -1) {
+		ki.kiTracker[kiNum][0] = goku.pos[0] + 50;
+		ki.kiTracker[kiNum][1] = goku.pos[1];
+	}
 }
 
 //Destroys the kiBlast, and 'unassigns' it.
 void kiFree(int kiID)
 {
-    ki.kiTracker[kiID][0] = UNASSIGN;
-    ki.kiTracker[kiID][1] = UNASSIGN;
+	ki.kiTracker[kiID][0] = UNASSIGN;
+	ki.kiTracker[kiID][1] = UNASSIGN;
 }
 
 //Updates the position of the kiBlast.
 void kiMove(int kiID)
 {
-    if (ki.kiTracker[kiID][0] > glob.xres/2) {
-        kiFree(kiID);
-    } else {
-        ki.kiTracker[kiID][0] += ki.kiVel;
-    }
+	if (ki.kiTracker[kiID][0] > glob.xres) {
+		kiFree(kiID);
+	} else {
+		ki.kiTracker[kiID][0] += ki.kiVel;
+	}
+	kiCollision(kiID);
 }
 
 //Handles graphics for the kiBlasts.
 void kiRender(int kiID)
 {
-    float cx = glob.xres/2.0;
-    float cy = glob.yres/2.0;
-    float h = 20.0;
-    float w = h*2;
-    glPushMatrix();
+	float h = 40.0;
+	float w = h*2;
+	glPushMatrix();
 
-    glTranslatef(ki.kiTracker[kiID][0], ki.kiTracker[kiID][1], 0);
-    glColor3f(1.0, 1.0, 1.0);
-    glBindTexture(GL_TEXTURE_2D, ki.image);
+	glTranslatef(ki.kiTracker[kiID][0], ki.kiTracker[kiID][1], 0);
+	glColor3f(1.0, 1.0, 1.0);
+	glBindTexture(GL_TEXTURE_2D, ki.image);
 
-    glEnable(GL_ALPHA_TEST);
-    glAlphaFunc(GL_GREATER, 0.0f);
-    glColor4ub(255,255,255,255);
+	glEnable(GL_ALPHA_TEST);
+	glAlphaFunc(GL_GREATER, 0.0f);
+	glColor4ub(255,255,255,255);
 
-    float tx = 0.0;
-    float ty = 0.0;
-    glBegin(GL_QUADS);
-    glTexCoord2f(tx+1,      ty+1); glVertex2i(cx-w, cy-h);
-    glTexCoord2f(tx+1,      ty);    glVertex2i(cx-w, cy+h);
-    glTexCoord2f(tx, ty);    glVertex2i(cx+w, cy+h);
-    glTexCoord2f(tx, ty+1); glVertex2i(cx+w, cy-h);
-    glEnd();
-    glPopMatrix();
-    glBindTexture(GL_TEXTURE_2D, 0);
-    glDisable(GL_ALPHA_TEST);
+	float tx = 0.0;
+	float ty = 0.0;
+	glBegin(GL_QUADS);
+	glTexCoord2f(tx+1, ty+1); 	glVertex2i(0, 0);
+	glTexCoord2f(tx+1, ty);    	glVertex2i(0, h);
+	glTexCoord2f(tx, ty);    	glVertex2i(w, h);
+	glTexCoord2f(tx, ty+1); 	glVertex2i(w, 0);
+	glEnd();
+	glPopMatrix();
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glDisable(GL_ALPHA_TEST);
 }
 
 /*
@@ -179,24 +279,74 @@ void kiRender(int kiID)
  * against that.
  * Enemy& enem
 */
-/*int kiCollision(Enemy& enem)
+void kiCollision(int kiRef)
 {
-    for (int i = 0; i < MAX_KI; i++) {
-
-    }
-    return 1;
-}*/
+	for (int i = 0; i < 3; i++) {
+		//printf("%f\t%d\t%d\n", enemyRef[i]->pos[0], ki.kiTracker[kiRef][0], glob.xres);
+		bool xColl = enemyRef[i]->pos[0] + 70 >= ki.kiTracker[kiRef][0]
+			&& ki.kiTracker[kiRef][0] + 15 >= enemyRef[i]->pos[0];
+		bool yColl = enemyRef[i]->pos[1] + 50 >= ki.kiTracker[kiRef][1]
+			&& ki.kiTracker[kiRef][1] + 30 >= enemyRef[i]->pos[1];
+		if (xColl && yColl) {
+			kiFree(kiRef);
+			break;
+		}
+	}
+}
 
 //Handler function to prevent repetitive code of parsing through the kiBlasts.
 void kiHandler(int type)
 {
-    for (int i = 0; i < MAX_KI; i++) {
-        if (ki.kiTracker[i][0] != UNASSIGN && ki.kiTracker[i][1] != UNASSIGN) {
-            if (type == 0) {
-                kiMove(i);
-            } else if (type == 1) {
-                kiRender(i);
-            }
-        }
-    }
+	for (int i = 0; i < MAX_KI; i++) {
+		if (ki.kiTracker[i][0] != UNASSIGN &&
+			ki.kiTracker[i][1] != UNASSIGN) {
+			if (type == 0) {
+				kiMove(i);
+			} else if (type == 1) {
+				kiRender(i);
+			}
+		}
+	}
+}
+
+//Sound handler, not functioning yet.
+void soundHandle()
+{
+	#ifdef SOUND
+		alutInit(0, NULL);
+		if (alGetError() != AL_NO_ERROR) {
+			printf("Error: alutInit()\n");
+		}
+		alGetError();
+
+		float vec[6] = {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f};
+		alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
+		alListenerfv(AL_ORIENTATION, vec);
+		alListenerf(AL_GAIN, 1.0f);
+		ALuint albuffer;
+		albuffer = alutCreateBufferFromFile("./sounds/supa sayain.mp3");
+
+		ALuint alSource;
+
+		alGenSources(1, &alSource);
+		alSourcei(alSource, AL_BUFFER, albuffer);
+		alSourcef(alSource, AL_GAIN, 1.0f);
+		alSourcef(alSource, AL_PITCH, 1.0f);
+		alSourcei(alSource, AL_LOOPING, AL_TRUE);
+		if (alGetError() != AL_NO_ERROR) {
+			printf("Error: setting source\n");
+		}
+		for (int i = 0; i < 4; i++) {
+			alSourcePlay(alSource);
+			usleep(250000);
+		}
+
+		alDeleteSources(1, &alSource);
+		alDeleteBuffers(1, &albuffer);
+		ALCcontext *Context = alcGetCurrentContext();
+		ALCdevice *Device = alcGetContextsDevice(Context);
+		alcMakeContextCurrent(NULL);
+		alcDestroyContext(Context);
+		alcCloseDevice(Device);
+	#endif
 }
